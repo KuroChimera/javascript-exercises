d2ab81e3ac8ea49612729d28de410542a73642e8
KuroChimera <kurocanidae@gmail.com>
2023-09-13 12:55:42 -0400

    Added script to sum a set of numbers in a range

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, numb2) {
+  if (isNaN(num1) || isNaN(numb2)){
+    return "ERROR";
+  }
+  if (num1 < 0 || numb2 < 0) {
+    return "ERROR"
+  }
+  let min = Math.min(num1, numb2);
+  let max = Math.max(num1, numb2);
+  let total = 0;
 
+  for (let i = min; i <= max; i++) {
+    total += i;
+  }
+  return total;
 };
 
 // Do not edit below this line
