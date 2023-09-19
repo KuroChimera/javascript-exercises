42b5e512659c5695dd89e26508542bf057b04208
KuroChimera <kurocanidae@gmail.com>
2023-09-19 14:26:57 -0400

    Corrected syntax

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,15 +1,14 @@
 const leapYears = function(year) {
-  if (year % 4 == 0) {
+  if (year % 4 === 0) {
     return true;
   }
-  if (year % 100 == 0) {
+  if (year % 100 === 0) {
     return false;
   }
-  if (year % 400 == 0) {
+  if (year % 400 === 0) {
     return true;
   } else {
     return false;
-  }
   }
 };
 
