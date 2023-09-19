16a93272cc772bb4b356cfbdf044714f87a148d7
KuroChimera <kurocanidae@gmail.com>
2023-09-19 13:52:27 -0400

    Fixed spelling and altered functions for check.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,7 @@
 const leapYears = function(year) {
-  if (year % 4 == 0 && year % 100 != 0 && year % 400 == 0) {
+  if (year % 4 == 0 || year % 400 == 0) {
     return true;
-  } 
-  else {
+  } else if (year % 100 == 0) {
     return false;
   }
 };
