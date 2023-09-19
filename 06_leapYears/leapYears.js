936747495c8d37478534ddfebd312151ba177590
KuroChimera <kurocanidae@gmail.com>
2023-09-19 14:22:02 -0400

    Revised conditionals and positioning of returns

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,15 @@
 const leapYears = function(year) {
-  if (year % 4 == 0 || year % 100 != 0 && year % 400 == 0) {
+  if (year % 4 == 0) {
+    return true;
+  }
+  if (year % 100 == 0) {
+    return false;
+  }
+  if (year % 400 == 0) {
     return true;
   } else {
     return false;
+  }
   }
 };
 
