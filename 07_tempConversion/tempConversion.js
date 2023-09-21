538329193918c159960fdb10f25beac3f80568bd
KuroChimera <kurocanidae@gmail.com>
2023-09-21 13:54:17 -0400

    Added variables for rounding temps

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
 const convertToCelsius = function(Fah) {
-  return (Fah - 32) * 5/9;
+  let celRound = (Fah - 32) * 5/9
+  return Math.round (celRound * 10) / 10;
 };
 
 const convertToFahrenheit = function(Cel) {
-  return (Cel * 9/5) + 32;
+  let fahRound = (Cel * 9/5) + 32;
+  return Math.round (fahRound * 10) / 10;
 };
 
 // Do not edit below this line
